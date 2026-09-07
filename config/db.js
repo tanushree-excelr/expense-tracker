0be33c4a349c1636f0b21731ca7f80c840124011
tanushree-excelr <tanushreediwan98@gmail.com>
2026-09-07 13:02:29 +0530

    feat: configure MongoDB connection with production URI and database utility module

--- config/db.js
+++ config/db.js
@@ -5,7 +5,7 @@ const connectDB = async () => {
     const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log('MongoDB Connected');
   } catch (error) {
-    console.error('MongoDB connection error');
+    console.error('MongoDB connection error:', error.message);
     process.exit(1);
   }
 };
